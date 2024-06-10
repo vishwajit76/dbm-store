d46da8016ac50088acc486a150b79830589fd761
neelmori <104917341+neelmori@users.noreply.github.com>
2024-06-10 15:38:19 +0530

    footer and about and contact form done

--- src/component/site/navbar.jsx
+++ src/component/site/navbar.jsx
@@ -22,7 +22,10 @@ import MailIcon from "@mui/icons-material/Mail";
 import { Grid, Menu, Modal, TextField } from "@mui/material";
 import logo from "../image/logo (1).png";
 import vector from "../image/Vector.png";
-import PhoneInput from "react-phone-input-2";
+import 'react-phone-input-2/lib/style.css';
+import PhoneInput from 'react-phone-input-2';
+import validator from 'validator';
+
 
 const style = {
   position: "absolute",
@@ -47,11 +50,14 @@ const Navbar = () => {
   const [open, setOpen] = React.useState(false);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+
   const [address, setAddress] = useState('');
   const [city, setCity] = useState('');
   const [state, setState] = useState('');
   const [country, setCountry] = useState('');
   const [zip, setZip] = useState('');
+  const [errors, setErrors] = useState({});
   const [profilePicture, setProfilePicture] = useState('/static/images/avatar/2.jpg');
 
   const handleFileChange = (e) => {
@@ -83,6 +89,61 @@ const Navbar = () => {
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
   };
+
+
+
+  const validateFields = () => {
+    const errors = {};
+
+    if (!validator.isAlpha(name.replace(/\s/g, ''))) {
+      errors.name = 'Name must contain only letters';
+    }
+
+    if (!validator.isEmail(email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    if (!validator.isNumeric(zip)) {
+      errors.zip = 'Zip code must contain only numbers';
+    }
+
+    if (!validator.isAlpha(state.replace(/\s/g, ''))) {
+      errors.state = 'State must contain only letters';
+    }
+
+    if (!validator.isAlpha(country.replace(/\s/g, ''))) {
+      errors.country = 'Country must contain only letters';
+    }
+
+    if (!validator.isAlpha(city.replace(/\s/g, ''))) {
+      errors.city = 'city must contain only letters';
+    }
+
+    return errors;
+  };
+
+  const handleSave = () => {
+    const validationErrors = validateFields();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      const profileData = {
+        name,
+        email,
+        address,
+        city,
+        state,
+        country,
+        zip,
+        phone,
+      };
+
+      localStorage.setItem('profileData', JSON.stringify(profileData));
+      handleCloseModal();
+    }
+  };
+
+
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
@@ -168,151 +229,170 @@ const Navbar = () => {
               <IconButton onClick={handleOpenModal}  sx={{ p: 0 }}>
                 <Avatar alt=""  src="/static/images/avatar/2.jpg"     sx={{ width: 35, height: 35 }} />
               </IconButton>
+
+
+
+              
               <Modal
-                open={openModal}
-                onClose={handleCloseModal}
-                aria-labelledby="parent-modal-title"
-                aria-describedby="parent-modal-description"
-              >
-                <Box
-                  sx={{
-                    ...style,
-                    width: '90%',
-                    maxWidth: { xs: 250, md: 600 },
-                    height: { xs: '70vh', md: 'auto' },
-                    overflow: 'auto',
+      open={openModal}
+      onClose={handleCloseModal}
+      aria-labelledby="parent-modal-title"
+      aria-describedby="parent-modal-description"
+    >
+      <Box
+        sx={{
+          ...style,
+          width: '90%',
+          maxWidth: { xs: 250, md: 600 },
+          height: { xs: '70vh', md: 'auto' },
+          overflow: 'auto',
+        }}
+      >
+        <Typography fontSize={'26px'} fontWeight={300} gutterBottom>
+          Edit Profile
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={3} sx={{ textAlign: 'center' }}>
+            <Avatar
+              alt="N"
+              src={profilePicture}
+              sx={{ width: 100, height: 100, margin: '0 auto', cursor: 'pointer' }}
+              onClick={() => document.getElementById('profile-picture-upload').click()}
+            />
+            <input
+              type="file"
+              accept="image/*"
+              style={{ display: 'none' }}
+              id="profile-picture-upload"
+              onChange={handleFileChange}
+            />
+          </Grid>
+          <Grid item xs={12} md={9}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="Name"
+                  value={name}
+                  variant="outlined"
+                  required
+                  error={!!errors.name}
+                  helperText={errors.name}
+                  onChange={(e) => setName(e.target.value)}
+                  size="small"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <PhoneInput
+                  inputStyle={{
+                    width: '100%',
+                    height: '40px',
+                    fontFamily: 'Monospace',
+                    border: '1px solid #AEB4BE',
                   }}
-                >
-                  <Typography fontSize={'26px'} fontWeight={300} gutterBottom>
-                    Edit Profile
-                  </Typography>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12} md={3} sx={{ textAlign: 'center' }}>
-                      <Avatar
-                        alt="D"
-                        src={profilePicture}
-                        
-                        sx={{ width: 100, height: 100, margin: '0 auto', cursor: 'pointer'  }}
-                        onClick={() => document.getElementById('profile-picture-upload').click()}
-                      />
-                      <input
-                        type="file"
-                        accept="image/*"
-                        style={{ display: 'none' }}
-                        id="profile-picture-upload"
-                        onChange={handleFileChange}
-                      />
-                    </Grid>
-                    <Grid item xs={12} md={9}>
-                      <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
-                          <TextField
-                            fullWidth
-                            label="Name"
-                            value={name}
-                            variant="outlined"
-                            required
-                            onChange={(e) => setName(e.target.value)}
-                            size="small"
-                          />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                          <PhoneInput
-                            inputStyle={{
-                              width: '100%',
-                              height: '40px',
-                              fontFamily: 'Monospace',
-                              border: '1px solid #AEB4BE',
-                            }}
-                            dropdownStyle={{}}
-                            country={"in"}
-                            variant="outlined"
-                            placeholder="+91"
-                          />
-                        </Grid>
-                        <Grid item xs={12}>
-                          <TextField
-                            fullWidth
-                            label="Email Address"
-                            value={email}
-                            required
-                            onChange={(e) => setEmail(e.target.value)}
-                            size="small"
-                          />
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <Grid container spacing={2}>
-                        <Grid item xs={12} md={6}>
-                          <TextField
-                            fullWidth
-                            label="Your Address"
-                            multiline
-                            rows={5}
-                            required
-                            value={address}
-                            onChange={(e) => setAddress(e.target.value)}
-                          />
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                          <Grid container spacing={2}>
-                            <Grid item xs={12}>
-                              <TextField
-                                fullWidth
-                                size="small"
-                                label="Your City"
-                                value={city}
-                                required
-                                onChange={(e) => setCity(e.target.value)}
-                              />
-                            </Grid>
-                            <Grid item xs={6}>
-                              <TextField
-                                fullWidth
-                                label="Your State"
-                                size="small"
-                                value={state}
-                                required
-                                onChange={(e) => setState(e.target.value)}
-                              />
-                            </Grid>
-                            <Grid item xs={6}>
-                              <TextField
-                                fullWidth
-                                label="Country"
-                                size="small"
-                                value={country}
-                                required
-                                onChange={(e) => setCountry(e.target.value)}
-                              />
-                            </Grid>
-                            <Grid item xs={6}>
-                              <TextField
-                                fullWidth
-                                label="Zip Code"
-                                size="small"
-                                value={zip}
-                                required
-                                onChange={(e) => setZip(e.target.value)}
-                              />
-                            </Grid>
-                            <Grid item xs={6}>
-                              <Button
-                                type="submit"
-                                variant="contained"
-                                fullWidth
-                              >
-                                Save
-                              </Button>
-                            </Grid>
-                          </Grid>
-                        </Grid>
-                      </Grid>
-                    </Grid>
+                  country={'in'}
+                  value={phone}
+                  onChange={(phone) => setPhone(phone)}
+                  inputProps={{
+                    name: 'phone',
+                    required: true,
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Email Address"
+                  value={email}
+                  required
+                  error={!!errors.email}
+                  helperText={errors.email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  size="small"
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  fullWidth
+                  label="Your Address"
+                  multiline
+                  rows={5}
+                  required
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField
+                      fullWidth
+                      size="small"
+                      label="Your City"
+                      value={city}
+                      required
+                      error={!!errors.city}
+                      helperText={errors.city}
+                      onChange={(e) => setCity(e.target.value)}
+                    />
                   </Grid>
-                </Box>
-              </Modal>
+                  <Grid item xs={6}>
+                    <TextField
+                      fullWidth
+                      label="Your State"
+                      size="small"
+                      value={state}
+                      required
+                      error={!!errors.state}
+                      helperText={errors.state}
+                      onChange={(e) => setState(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      fullWidth
+                      label="Country"
+                      size="small"
+                      value={country}
+                      required
+                      error={!!errors.country}
+                      helperText={errors.country}
+                      onChange={(e) => setCountry(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      fullWidth
+                      label="Zip Code"
+                      size="small"
+                      value={zip}
+                      required
+                      error={!!errors.zip}
+                      helperText={errors.zip}
+                      onChange={(e) => setZip(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      fullWidth
+                      onClick={handleSave}
+                    >
+                      Save
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+    </Modal>
             </Box>
             <Box sx={{ display: { xs: "flex", md: "none" } }}>
               <IconButton
---
+++ src/component/user/Footer.jsx
@@ -0,0 +1,106 @@
+import { Box, Button, Container, Grid, Stack, TextField, Typography } from '@mui/material'
+import React from 'react'
+import img from '../image/logo (1).png'
+function Footer() {
+
+    const handleEmailClick = () => {
+        window.location.href = 'mailto:info@digibulkmarketing.com';
+      };
+    
+      const handlePhoneClick = () => {
+        window.location.href = 'tel:18008898358';
+      };
+    
+  return (
+    
+       <>
+      <Container>
+        <Grid container spacing={4} marginTop={10}>
+          <Grid item xs={12} sm={6}>
+            <Box display="flex" justifyContent={{ xs: 'center', sm: 'flex-start' }}>
+              <img src={img} alt="Logo" style={{ maxWidth: '40%', height: 'auto' }} />
+            </Box>
+            <Box mt={4}>
+              <Typography>Address: B204, Sumel Business Park – 7,</Typography>
+              <Typography>Odhav, Ahmedabad – 382415</Typography>
+              <Box display="flex" flexDirection='column' justifyContent='flex-start'>
+                <Typography mt={4} onClick={handleEmailClick} style={{ cursor: 'pointer', color: 'black' }}>
+                  Email: info@digibulkmarketing.com
+                </Typography>
+                <Typography mt={0} onClick={handlePhoneClick} style={{ cursor: 'pointer', color: 'black' }}>
+                  Phone: 1800-889-8358
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+
+          <Grid item xs={12} sm={3} >
+            <Box display="flex" justifyContent={{ xs: 'center', sm: 'flex-start' }}>
+              <Box marginTop={{ xs: '20px', sm: 0 }} textAlign='left' justifyContent={'center'} alignItems={'center'}>
+                <Typography fontWeight={900}>
+                  USEFUL LINKS
+                </Typography>
+                <Box mt={3}>
+                  <ul style={{ listStyleType: 'inherit', padding: 0, margin: 0}}>
+                    <li style={{ listStylePosition: 'inside', marginBottom: '0px',cursor: 'pointer' }}>Download</li>
+                    <li style={{ listStylePosition: 'inside', cursor: 'pointer' }}>Shop</li>
+                    <li style={{ listStylePosition: 'inside',cursor: 'pointer'  }}>Orders</li>
+                    <li style={{ listStylePosition: 'inside', cursor: 'pointer' }}>Reseller</li>
+                    <li style={{ listStylePosition: 'inside',cursor: 'pointer'  }}>FAQ</li>
+                  </ul>
+                </Box>
+              </Box>
+            </Box>
+          </Grid>
+
+          <Grid item xs={12} sm={3}>
+            <Box display="flex" justifyContent={{ xs: 'center', sm: 'flex-start' }}>
+              <Box marginTop={{ xs: '20px', sm: 0 }} textAlign={{ xs: 'center', sm: 'left' }}>
+                <Typography fontWeight={800} marginRight={'65px'}>
+                  NEWSLETTER
+                </Typography>
+                <Stack spacing={0} mt={1}>
+                  <TextField variant='standard' color='black' label='Enter your Name' fullWidth />
+                  <TextField variant='standard' color='black' label='Enter your email' fullWidth />
+                  <Button sx={{
+                    mt: 3,
+                    width: '50%',
+                    backgroundColor: '#',
+                    color: 'white'
+                  }} type="submit" variant="contained">
+                    SUBSCRIBE
+                  </Button>
+                </Stack>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+      <Box sx={{ backgroundColor: '#8C8C8C' }} margin={' 50px 0 0 0 '} padding={' 20px 0'}>
+        <Container>
+          <Grid container display={'flex'}>
+            <Grid item xs={12} md={6}>
+
+              <Typography fontSize={'14px'} sx={{ color: "#fff", alignItems: 'center' }}>
+                Copyright © Designed & Developed by BITBEAST PRIVATE LIMITED 2023
+              </Typography>
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <Box display={"flex"} sx={{ flexDirection: { xs: 'column', sm: "row" }, alignItems: 'center', justifyContent: 'space-between' }}>
+                <Typography sx={{ color: '#fff',cursor: 'pointer' }}>Download</Typography>
+                <Typography sx={{ color: '#fff', cursor: 'pointer' }}>Shop</Typography>
+                <Typography sx={{ color: '#fff', cursor: 'pointer' }}>Orders</Typography>
+                <Typography sx={{ color: '#fff', cursor: 'pointer' }}>Reseller</Typography>
+                <Typography sx={{ color: '#fff', cursor: 'pointer' }}>FAQ</Typography>
+              </Box>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </>
+
+  )
+}
+
+export default Footer
